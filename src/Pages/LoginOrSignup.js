97e12486c1e4b58a7e4b86bc525f258ff776c629
Joni Juntto <joni.juntto@mustigroup.com>
2022-01-26 19:30:52 +0200

    added sign in option for google

--- src/Pages/LoginOrSignup.js
+++ src/Pages/LoginOrSignup.js
@@ -4,16 +4,19 @@ import HeaderHome from '../components/HeaderHome';
 import SocialArray from '../components/SocialArray';
 import ModalLoginBig from '../components/ModalLoginBig';
 import background from '../assets/background.jpg'
-import { Typography } from '@material-ui/core';
+import { Button, Typography } from '@material-ui/core';
 import '../styles.css/LoginOrSignUp.css';
 import loginButton from '../assets/loginbutton.png'
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useHistory } from "react-router-dom";
 
 
 
 export default function LoginOrSignup() {
+
+    const history = useHistory();
 
     const notify = (text, state) =>{
         if(state === 'success'){
@@ -29,6 +32,9 @@ export default function LoginOrSignup() {
         try {
           await signInWithPopup(auth, provider);
           notify('Login success', 'success');
+          // eslint-disable-next-line no-restricted-globals
+          history.push('/create');
+          console.log(provider, auth)
         }
         catch (error) {
           const errorMessage = error.message;
@@ -38,22 +44,14 @@ export default function LoginOrSignup() {
     
 
     return (
-
         <div className="LoginOrSignUp">
-            <HeaderHome />
             <div>
                 <ToastContainer />
-                <div style={{
-                    position: 'absolute', left: '50%', top: '50%',
-                    transform: 'translate(-50%, -50%)'
-                }}>
                     <Typography style={{marginBottom: '50px'}} variant='h4'>Löydä peliseuraa omiin lempipeleihin!</Typography>
                     <ModalLoginBig />
                     <Typography variant='h3'>Tai</Typography>
-                    <img src={loginButton} onClick={signInWithGoogle}/>
+                    <Button onClick={signInWithGoogle}>Button</Button>
                 </div >
-                <SocialArray />
-            </div>
         </div>
 
     );
--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -6,10 +6,11 @@ import { Avatar, Paper, Typography, Box, Divider } from '@material-ui/core';
 import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
 import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 import GroupIcon from '@mui/icons-material/Group';
-import {logout} from '../firebase';
+import { logout } from '../firebase';
 import { useHistory } from "react-router-dom";
 import { Button } from "@material-ui/core";
 import DeleteIcon from '@mui/icons-material/Delete';
+import '../App.css';
 
 export default function Profile() {
     const currentUser = useAuth();
@@ -17,47 +18,73 @@ export default function Profile() {
     const [notifs, setNotifs] = useState([]);
     const history = useHistory();
 
-    const logoutF = async () =>{
+    const logoutF = async () => {
         try {
-          await logout();
-          history.push('/loggedout')
+            await logout();
+            history.push('/loggedout')
         } catch (error) {
-          
+
         }
-        
-      }
+
+    }
 
     const getUserData = async () => {
-        console.log("getting user data for user " + currentUser.uid)
-        const docRef = doc(db, "users", currentUser.uid);
-        const docSnap = await getDoc(docRef);
+        try {
+            console.log("getting user data for user " + currentUser.uid)
+            const docRef = doc(db, "users", currentUser.uid);
+            const docSnap = await getDoc(docRef);
 
-        if (docSnap.exists()) {
-            console.log("Document data:", docSnap.data());
-            setPerson(docSnap.data());
-        } else {
-            // doc.data() will be undefined in this case
-            console.log("No such document!");
+            if (docSnap.exists()) {
+                console.log("Document data:", docSnap.data());
+                setPerson(docSnap.data());
+            } else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
+                throw new Error('No user')
+            }
+        } catch (error) {
+            console.log(error)
         }
+
     }
     const getUserNotifs = async () => {
-        const docRef = doc(db, "userNotifs", currentUser.uid);
-        const docSnap = await getDoc(docRef);
+        try {
+            const docRef = doc(db, "userNotifs", currentUser.uid);
+            const docSnap = await getDoc(docRef);
 
-        if (docSnap.exists()) {
-            console.log("Document data:", docSnap.data());
-            const notificationArr = docSnap.data();
-            const notifsArr = notificationArr.notifications;
-            console.log(notifsArr.msg)
-            setNotifs(notifsArr.msg);
-        } else {
-            // doc.data() will be undefined in this case
-            setNotifs({
-                msg: ["no messages"],
-                senderID: ["no sender ids"]
-            })
-            console.log("No such document!");
+            if (docSnap.exists()) {
+                console.log("Document data:", docSnap.data());
+                const notificationArr = docSnap.data();
+                const notifsArr = notificationArr.notifications;
+                console.log(notifsArr.msg)
+                setNotifs(notifsArr.msg);
+            } else {
+                // doc.data() will be undefined in this case
+                setNotifs({
+                    msg: ["no messages"],
+                    senderID: ["no sender ids"]
+                })
+                console.log("No such document!");
+                throw new Error("No document")
+            }
+        } catch (error) {
+            console.log(error)
         }
+
+    }
+    const nav = () =>{
+        history.push('/create')
+    }
+
+    const EmptyPage = () =>{
+        return(
+            <div>
+                <header className="App-header">
+                    <Typography variant="h3">This is empty, because you have no profile</Typography>
+                    <Button onClick={nav}>Create profile</Button>
+                </header>
+            </div>
+        );
     }
 
     useEffect(() => {
@@ -67,45 +94,53 @@ export default function Profile() {
         }
     }, [currentUser]);
 
-    const deleteNotification = async (noti) =>{
+    const deleteNotification = async (noti) => {
         alert("notification " + noti + " deleted")
     }
 
     return (
-        <div>
-            <Header />
-            <Button onClick={logoutF}>Kirjaudu ulos</Button>
-            <div style={{ justifyContent: 'center', display: 'flex', flexDirection: 'row' }}>
-                <Paper elevation={5} style={{ padding: 23, borderRadius: 23, marginTop: "5vh", flexDirection: 'column', width: '40em', display: 'flex' }}>
-                    <Box style={{}}>
-                        <h3 style={{ alignSelf: 'flex-start' }}>{person.name}</h3>
-                        <Avatar alt={person.name} src={person.url} style={{ width: '8em', height: '8em' }} />
+        <div>{person?.id
+        ?<div>
+         <Header />
+            <div>
+                <Button onClick={logoutF}>Kirjaudu ulos</Button>
+                <div style={{ justifyContent: 'center', display: 'flex', flexDirection: 'row' }}>
+                    <Paper elevation={5} style={{ padding: 23, borderRadius: 23, marginTop: "5vh", flexDirection: 'column', width: '40em', display: 'flex' }}>
+                        <Box style={{}}>
+                            <h3 style={{ alignSelf: 'flex-start' }}>{person.name}</h3>
+                            <Avatar alt={person.name} src={person.url} style={{ width: '8em', height: '8em' }} />
 
-                        <Typography variant='h5'>Kuvaus: {person.bio}</Typography>
-                    </Box>
-                    <Divider />
-                    <Box style={{}}>
-                        <NotificationsActiveIcon />
-                        <Typography variant='h5'>Ilmoitukset</Typography>
-                        <div>{notifs
-                        ? notifs.map((noti)=>(
-                            <Paper>
-                                <Typography variant='p'>{noti}</Typography>
-                                <DeleteIcon color='error'onClick={()=>deleteNotification(noti)}/>
-                                 </Paper>
-                        ))
-                        :<Typography >Ei uusia ilmoituksia</Typography>
-                        }
-                        </div>
-                        <SportsEsportsIcon />
-                        <Typography variant='h5' style={{ margin: 10 }}>Lempi peli: {person.latestGame}</Typography>
-                        <GroupIcon />
-                        <Typography variant='h5'>Kavereita: 100</Typography>
-                    </Box>
-                </Paper>
+                            <Typography variant='h5'>Kuvaus: {person.bio}</Typography>
+                        </Box>
+                        <Divider />
+                        <Box style={{}}>
+                            <NotificationsActiveIcon />
+                            <Typography variant='h5'>Ilmoitukset</Typography>
+                            <div>{!notifs
+                                ? notifs.map((noti) => (
+                                    <Paper>
+                                        <Typography variant='p'>{noti}</Typography>
+                                        <DeleteIcon color='error' onClick={() => deleteNotification(noti)} />
+                                    </Paper>
+                                ))
+                                : <Typography >Ei uusia ilmoituksia</Typography>
+                            }
+                            </div>
+                            <SportsEsportsIcon />
+                            <Typography variant='h5' style={{ margin: 10 }}>Lempi peli: {person.latestGame}</Typography>
+                            <GroupIcon />
+                            <Typography variant='h5'>Kavereita: 100</Typography>
+                        </Box>
+                    </Paper>
+
+                </div>
 
             </div>
-
+            </div>
+            :<EmptyPage />
+            
+            }
+           
         </div>
     );
 }
